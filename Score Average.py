7b6b83695689bcb1da0dc147f342c21f3616acbb
ngenardi <ngenardi@gmail.com>
2020-09-30 16:51:15 -0400

    Score Average V1.1
    First working version of score averaging script.

--- Score Average.py
+++ Score Average.py
@@ -1,2 +1,12 @@
-NumberOfScores = input("Insert number of scores to be averaged: ")
-print(NumberOfScores)
+NumberOfScores = int(input("Input number of scores to be averaged: "))
+Score = SumOfScores = Count = 0
+
+while(NumberOfScores > Count):
+    Score = int(input("Input a score to be averaged: "))
+    Count+=1
+    SumOfScores += Score
+    if(NumberOfScores > Count):
+        print("The average of scores so far is " + str(float(SumOfScores/Count)) + " with " + str(Count) + " of " + str(NumberOfScores) + " inputted.")
+
+AverageOfScores = float(SumOfScores/NumberOfScores)
+print("The final average of all " + str(NumberOfScores) + " scores inputted is " + str(AverageOfScores) + ".")
