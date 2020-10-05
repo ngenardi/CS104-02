ee96f87b6ccd9329bbaf40eb3d9a6199b4419953
ngenardi <ngenardi@gmail.com>
2020-10-05 16:37:20 -0400

    Conditions v1.1
    Edited input to add instruction to end code.

--- conditions.py
+++ conditions.py
@@ -1,5 +1,5 @@
 while True:
-    temp = int(input("Enter the temperature in degrees fahrenheit: "))
+    temp = int(input("Enter the temperature in degrees fahrenheit or enter \"999\" to end the program: "))
 
     if temp == 999:
         break
